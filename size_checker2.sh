dac35788af01a3227c2d1f8d74ae48485e0180a0
evendoom <davidej.p.correia@gmail.com>
2023-08-14 16:53:39 +0000

    update read/write paths

--- size_checker2.sh
+++ size_checker2.sh
@@ -7,9 +7,9 @@
 
 # *** Declare variables ***
 # Get absolute path of Projects Root
-ABS_PATH='/workspace/playground/ads/volume_2/projects/'
+ABS_PATH='/co3fs/Projects/'
 # Create an array with all project folders
-readarray -t PROJECT_LIST <<< $(ls -l /workspace/playground/ads/volume_2/projects/ | grep -v 'total' | awk '{print $9}')
+readarray -t PROJECT_LIST <<< $(ls -l /co3fs/Projects/ | grep -v 'total' | awk '{print $9}')
 
 # *** Declare functions ***
 # CD onto project folder, size check 1st and 2nd levels of subfolders
@@ -24,10 +24,10 @@ check_size() {
     echo "Size checking ${PROJECT_ROOT}..."
 
     cd "${TOTAL_PATH}"
-    echo "You're in $(pwd)" | tee -a /workspace/playground/ads/volume_2/projects/results_"${PROJECT_ROOT}".txt
-    du -skhc * | tee -a /workspace/playground/ads/volume_2/projects/results_"${PROJECT_ROOT}".txt
-    FOLDER_TOTAL=$(grep -i "total" /workspace/playground/ads/volume_2/projects/results_"${PROJECT_ROOT}".txt | awk '{print $1}')
-    echo "${PROJECT_ROOT},${FOLDER_TOTAL},${TOTAL_PATH}" >> /workspace/playground/ads/volume_2/projects/results.csv
+    echo "You're in $(pwd)" | tee -a /co3fs/data_io/davide/size_checks/results_"${PROJECT_ROOT}".txt
+    du -skhc * | tee -a /co3fs/data_io/davide/size_checks/results_"${PROJECT_ROOT}".txt
+    FOLDER_TOTAL=$(grep -i "total" /co3fs/data_io/davide/size_checks/results_"${PROJECT_ROOT}".txt | awk '{print $1}')
+    echo "${PROJECT_ROOT},${FOLDER_TOTAL},${TOTAL_PATH}" >> /co3fs/data_io/davide/size_checks/results.csv
 
     # Check level 2 of subfolders
     for subfolder in "${!LIST_OF_SUBFOLDERS[@]}"
@@ -35,8 +35,8 @@ check_size() {
             if [[ -d "${TOTAL_PATH}/${LIST_OF_SUBFOLDERS[subfolder]}" ]]
             then
                 cd "${TOTAL_PATH}/${LIST_OF_SUBFOLDERS[subfolder]}"
-                echo "You're in $(pwd)" | tee -a /workspace/playground/ads/volume_2/projects/results_"${PROJECT_ROOT}".txt
-                du -skhc * | tee -a /workspace/playground/ads/volume_2/projects/results_"${PROJECT_ROOT}".txt
+                echo "You're in $(pwd)" | tee -a /co3fs/data_io/davide/size_checks/results_"${PROJECT_ROOT}".txt
+                du -skhc * | tee -a /co3fs/data_io/davide/size_checks/results_"${PROJECT_ROOT}".txt
             else
                 echo "Couldn't find ${TOTAL_PATH}/${LIST_OF_SUBFOLDERS[subfolder]}"
                 echo "Skipping..."
@@ -46,8 +46,8 @@ check_size() {
 }
 
 # Initiate CSV file
-touch /workspace/playground/ads/volume_2/projects/results.csv
-echo "project_name,size,path" >> /workspace/playground/ads/volume_2/projects/results.csv
+touch /co3fs/data_io/davide/size_checks/results.csv
+echo "project_name,size,path" >> /co3fs/data_io/davide/size_checks/results.csv
 
 # Loop through PROJECT LIST array
 for project in "${!PROJECT_LIST[@]}"
