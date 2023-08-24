97bf087dd89caa7eb1e3d142d405940d19579670
evendoom <davidej.p.correia@gmail.com>
2023-08-24 17:01:08 +0000

    update apple_md5

--- apple_md5.sh
+++ apple_md5.sh
@@ -13,13 +13,37 @@ then
     exit 1
 fi
 
+# Create functions
+# Check if exit status is 0
+check_status() {
+    MESSAGE="${1}"
+    if [[ "${?}" -ne 0 ]]
+    then
+        echo "${MESSAGE}"
+        exit 1
+    fi
+}
+
 # Create variables
 SOURCE_PATH="${1}"
 MD5_NAME="${2}"
 
 # CD onto top level
-cd "${1}"../
+cd "${1}"/../
 echo "You're in $(pwd)"
 
 # Create standard MD5
+echo "Creating MD5 file..."
 find "${1}" -type f -exec md5sum {} >> ./"${2}_standard.md5" \;
+
+# Check if exit status is 0
+check_status "Something went wrong creating MD5, aborting..."
+
+# Parse MD5 to meet Apple standards
+cat "${1}/../${2}_standard.md5" | sort -k 2 | awk '{print $2"|"$1}' >> ./"${2}.md5"
+
+# Check if exit status is 0
+check_status "Failed parsing, aborting..."
+
+# Clean up
+rm -vf "${1}/${2}_standard.md5"
