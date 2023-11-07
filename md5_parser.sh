c0f8c5b7dee9b17d15a604a62afe0096fb1a9a83
evendoom <davidej.p.correia@gmail.com>
2023-11-07 12:40:45 +0000

    feat: Update md5_parser

--- md5_parser.sh
+++ md5_parser.sh
@@ -13,6 +13,7 @@ fi
 
 # Create Variables
 SOURCE="${1}"
+MD5_NAME=$(basename "${1}")
 
 # Find MD5 files and store them in an array
 readarray -t MD5_LIST <<< $(find "${1}" -iname "*.md5" -type f)
@@ -21,8 +22,16 @@ readarray -t MD5_LIST <<< $(find "${1}" -iname "*.md5" -type f)
 for file in "${!MD5_LIST[@]}"
     do
         # Remove "./" from each entry, add absolute path to each checksum and store all entries on "${SOURCE}" as collated_file.md5
-        cat "${MD5_LIST[file]}" | sed 's\./\\' | awk -v source="${SOURCE}" '{$2=source$2; print $1 "  " $2}' >> "${SOURCE}"collated_file.md5
+        cat "${MD5_LIST[file]}" | sed 's\./\\' | awk -v source="$(dirname "${MD5_LIST[file]}")" '{$2=source"/"$2; print $1 "  " $2}' >> /workspace/playground/ads/tmp/"${MD5_NAME}".md5
     done
 
-# Run md5sum -c on collated_file
-md5sum -c "${SOURCE}"collated_file.md5
+# Check if Array iteration was successful
+if [[ "${?}" -ne 0 ]]
+then
+    echo "Something went wrong parsing MD5 files"
+    echo "Exiting..."
+    exit 1
+fi
+
+# Run md5sum -c on collated MD5 file
+md5sum -c /workspace/playground/ads/tmp/"${MD5_NAME}".md5
